10c2462bf5735923cf21a02ce3be25069b2880d0
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2024-07-29 21:34:39 -0300

    Start

---
+++ src/main/java/com/cbtc/cbtc/CbtcApplication.java
@@ -0,0 +1,13 @@
+package com.cbtc.cbtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbtc/cbtc/CbtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbtc.cbtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
