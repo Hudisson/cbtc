41351e357b5ead9302789f6772ec099291e403af
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2024-08-11 04:00:36 -0300

    Inicializado o desenvolvimento do método de busca na API externa para coletar a contação do BTC

--- src/main/java/com/cbtc/cbtc/controllers/HistoricoBTCBRLController.java
+++ src/main/java/com/cbtc/cbtc/controllers/HistoricoBTCBRLController.java
@@ -3,19 +3,26 @@ package com.cbtc.cbtc.controllers;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.text.DecimalFormat;
+import java.time.Duration;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cbtc.cbtc.repositories.HistoricoBTCBRLRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HistoricoBTCBRLController {
@@ -23,13 +30,16 @@ public class HistoricoBTCBRLController {
     @Autowired
     HistoricoBTCBRLRepository hBtcBrlRepository;
 
-    public void HttpQuotationBTCBRL() throws IOException, InterruptedException{
+    public String HttpQuotationBTCBRL() throws IOException, InterruptedException{
         HttpRequest request = (HttpRequest) HttpRequest.newBuilder()
         .header("Accept", "application/json")
+        .timeout(Duration.ofSeconds(5))
 		.GET().uri(URI.create("https://api.coinbase.com/v2/prices/BTC-BRL/spot"))
 		.build();
 
-        HttpClient clientJava = HttpClient.newBuilder().build();
+        HttpClient clientJava = HttpClient.newBuilder()
+        .connectTimeout(Duration.ofSeconds(5))
+        .build();
         HttpResponse<String> response  = clientJava.send(request, BodyHandlers.ofString());
 
         JSONObject jsonObj = new JSONObject(response.body());
@@ -42,12 +52,18 @@ public class HistoricoBTCBRLController {
 
         DecimalFormat df = new DecimalFormat("###,###.##");
         String vf = df.format(preco);
-        System.out.println("Preço R$:"+vf);
+        return vf;
     }
 
-    @GetMapping("/price")
-    public void teste() throws IOException, InterruptedException{
-        HttpQuotationBTCBRL();
-    }
+   @GetMapping("/buscar")
+   public ResponseEntity<String> buscar() throws Exception{
+
+        String resultado =  HttpQuotationBTCBRL();
+
+        return ResponseEntity.ok().body("BTC-BRL - R$: "+resultado);
+            
+   }
+
+
 }
 
